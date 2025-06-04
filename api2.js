a4de7c261d2512a138f76b6a01c3fe04d375cbc5
Bhavna Sharma <bhavna@i2k2.com>
2025-06-04 19:36:15 +0530

    implementation of ejs!!

--- api2.js
+++ api2.js
@@ -2,11 +2,20 @@ const fs = require("fs");
 const path = require("path");
 const express = require('express');
 const app = express();
+app.set('view engine', 'ejs')
 const publicPath = path.join(__dirname, 'public')
 console.log(publicPath)
 // app.use(express.static(publicPath));
 app.get('', (req, resp) => {
     resp.sendFile(`${publicPath}/home.html`)
+});
+app.get('/profile', (req, resp) => {
+    const user = {
+        name: 'Bhavna',
+        email: 'test@gmail.com',
+        number:'7895030785'
+    }
+    resp.render('profile',{user})
 });
 app.get('/about', (req, resp) => {
     resp.sendFile(`${publicPath}/about.html`)
