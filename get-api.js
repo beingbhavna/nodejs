3c5a3a3a9855ea3e444daa232e8d087aac63bb36
Bhavna Sharma <bhavna@i2k2.com>
2025-06-13 19:22:24 +0530

    updated

--- get-api.js
+++ get-api.js
@@ -1,1 +1,12 @@
 const express = require('express');
+const dbConnect = require('./mongodb-connection');
+const app = express();
+
+
+ app.get('', async(req,resp)=>{
+    let db =  await dbConnect();
+    let data = await db.find().toArray();
+    resp.send(data)
+    console.log(data)
+});
+app.listen(5600);
